757ba15be8f12f741fdc087e28a3201f67edb2e5
AlonsoAliaga <36780081+AlonsoAliaga@users.noreply.github.com>
2025-05-21 19:59:08 -0500

    Updated index.html

--- docs/script.js
+++ docs/script.js
@@ -1,12 +1,3 @@
 function test() {
   console.log("TESTING WORKS!")
 }
-function toggleDarkmode() {
-    if (document.getElementById('darkmode').checked == true) {
-      document.body.classList.add('dark');
-    } else {
-      document.body.classList.remove('dark');
-    }
-    //console.log("Dark mode is now: "+(document.getElementById('darkmode').checked))
-}
-toggleDarkmode();
